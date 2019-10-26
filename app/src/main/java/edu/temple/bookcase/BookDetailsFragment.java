fc0d3aaf5d15db2b14955e861947f6c3caa5776b
Jeslin Samuel <jeslin.samuel@temple.edu>
2019-10-26 18:37:55 -0400

    Created fragments and corresponding layouts.

---
+++ app/src/main/java/edu/temple/bookcase/BookDetailsFragment.java
@@ -0,0 +1,25 @@
+package edu.temple.bookcase;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BookDetailsFragment extends Fragment
+{
+    public static BookDetailsFragment newInstance()
+    {
+        BookDetailsFragment bookDetailsFragment = new BookDetailsFragment();
+        return bookDetailsFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/edu/temple/bookcase/BookListFragment.java
@@ -0,0 +1,25 @@
+package edu.temple.bookcase;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BookListFragment extends Fragment
+{
+    public static BookListFragment newInstance()
+    {
+        BookListFragment bookListFragment = new BookListFragment();
+        return bookListFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
