a59da1155a0f2fbd2168115f1e5c2ab885c09c78
Jeslin Samuel <jeslin.samuel@temple.edu>
2019-10-29 18:48:56 -0400

    Fleshed out ViewPagerFragment to contain standard code for a ViewPager.

--- app/src/main/java/edu/temple/bookcase/ViewPagerFragment.java
+++ app/src/main/java/edu/temple/bookcase/ViewPagerFragment.java
@@ -8,9 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 public class ViewPagerFragment extends Fragment
 {
+    ViewPager viewPager;
+    ArrayList<BookDetailsFragment> fragments;
+
     public static ViewPagerFragment newInstance()
     {
         ViewPagerFragment viewPagerFragment = new ViewPagerFragment();
@@ -20,6 +28,39 @@ public class ViewPagerFragment extends Fragment
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_bookdetails, container, false);
+
+        fragments = new ArrayList<BookDetailsFragment>();
+
+        fragments.add(BookDetailsFragment.newInstance("Huckleberry Finn"));
+
+        viewPager = view.findViewById(R.id.viewPager);
+
+        CustomViewPagerAdapter adapter = new CustomViewPagerAdapter(getChildFragmentManager(), fragments);
+
+        viewPager.setAdapter(adapter);
+
+        return view;
+    }
+
+    class CustomViewPagerAdapter extends FragmentStatePagerAdapter
+    {
+        ArrayList<BookDetailsFragment> fragments;
+
+        public CustomViewPagerAdapter(@NonNull FragmentManager fm, ArrayList<BookDetailsFragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 }
