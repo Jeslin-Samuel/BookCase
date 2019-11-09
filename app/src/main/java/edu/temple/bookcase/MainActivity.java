13aae9f6485afdc45c04a1f5d8672f8316d49517
Jeslin Samuel <jeslin.samuel@temple.edu>
2019-11-08 19:04:18 -0500

    Added thread that processes HTTP request and obtains JSON Object, which then prints it to the log.

--- app/src/main/java/edu/temple/bookcase/MainActivity.java
+++ app/src/main/java/edu/temple/bookcase/MainActivity.java
@@ -3,7 +3,11 @@ package edu.temple.bookcase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.os.Message;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -18,6 +22,36 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
         ViewPagerFragment viewPagerFragment;
         BookDetailsFragment bookDetailsFragment;
         ArrayList<String> bookList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.list_books)));
+
+        new Thread() {
+            @Override
+            public void run()
+            {
+                URL url;
+                try
+                {
+                    url = new URL("https://kamorris.com/lab/audlib/booksearch.php");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                    StringBuilder builder = new StringBuilder();
+                    String response;
+
+                    while ((response = reader.readLine()) != null) {
+                        builder.append(response);
+                    }
+
+                    Message message = Message.obtain();
+                    message.obj = builder.toString();
+
+                    Log.d("JSON Data", message.obj.toString());
+
+                }
+
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
         if ((findViewById(R.id.landscape) == null) && (findViewById(R.id.large) == null))
         {
@@ -42,4 +76,6 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
         BookDetailsFragment bookDetailsFragment = (BookDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.rightHalf);
         bookDetailsFragment.textView.setText(title);
     }
+
+    public void booklistJSONGet(String myURL) {}
 }
